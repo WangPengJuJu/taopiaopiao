8679b3e7ba65c087454f822575827136bf2534e7
unknown <759161168@qq.com>
2018-10-15 22:45:24 +0800

    增加postcss,图片打包等配置

---
+++ src/components/Apptips.js
@@ -0,0 +1,16 @@
+import React from 'react';
+export class Apptips extends React.Component{
+    constructor(props){
+        super(props)
+    }
+    render(){
+        return(
+            <div>
+                <span>app</span>
+                <img className="thumb-img"/>
+            </div>
+        )
+    }
+    componentDidMout(){}
+
+}
--- src/index.js
+++ src/index.js
@@ -1,7 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { render } from 'react-dom';
-
+import {Apptips} from './components/Apptips';
+import './css/index.css';
+class App extends React.Component{
+    render(){
+        return(
+            <Apptips></Apptips>
+        )
+    }
+}
 render(
-    <div>Hello React!</div>,
+    <App/>,
     document.getElementById('app')
 );
